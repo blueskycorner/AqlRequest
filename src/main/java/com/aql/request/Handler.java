bfd2c6e2096d4409b315711bd832868220b8f724
SESA401561 <SESA401561@W7FRR04774L.eur.gad.schneider-electric.com>
2017-10-12 22:49:33 +0200

    Modify profile Add DynamoDB permission Code putItem

--- src/main/java/com/aql/request/Handler.java
+++ src/main/java/com/aql/request/Handler.java
@@ -1,22 +1,20 @@
 package com.aql.request;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Iterator;
-
 import org.apache.log4j.Logger;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.aql.request.AqlRequest;
-import com.aql.request.RequestContext;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import java.util.regex.*;
 
@@ -26,38 +24,57 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
         private static final ObjectMapper objectMapper = new ObjectMapper();
 	private DynamoDB dynamoDb;
     	private String DYNAMODB_TABLE_NAME = "aqlRequest";
-	private com.amazonaws.regions.Regions REGION = Regions.EU_WEST_1;
+	private Regions REGION = Regions.EU_WEST_1;
 
 	private void Record(AqlRequest r, String ip, long date)
 	{
 		AmazonDynamoDBClient client = new AmazonDynamoDBClient();
-        client.setRegion(com.amazonaws.regions.Region.getRegion(REGION));
+        client.setRegion(Region.getRegion(REGION));
         this.dynamoDb = new DynamoDB(client);
-
+        LOG.info("Connected to dynamo");
+        Table t = dynamoDb.getTable(DYNAMODB_TABLE_NAME);
+        LOG.info("dynamo table retrieved");
+        t.putItem(new PutItemSpec().withItem(new Item()
+                        .withLong("timestamp", date)
+                        .withString("email", r.getEmail())
+                        .withString("ip", ip)
+                        .withString("app_lib_version", r.getApp_lib_version())
+                        .withString("core_lib_version", r.getCore_lib_version())
+                        .withString("standard", r.getRequestData().getStandard())
+                        .withLong("batch_size", r.getRequestData().getBatch_size())
+                        .withString("code_letter", r.getRequestData().getCode_letter())
+                        .withString("type", r.getRequestData().getType())
+                        .withString("procedure", r.getRequestData().getProcedure())
+                        .withString("aql", r.getRequestData().getAql())
+                        ));
+        LOG.info("PutItem OK");
 	}
 
 	@Override
-	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) 
+	{
 		LOG.info("received: " + input);
         AqlRequest r = null;
-	String ip = null;
+        String ip = null;
         try
         {
             r = objectMapper.readValue(input.get("body").toString(), AqlRequest.class);
 	
-	    String sRequestContext = input.get("requestContext").toString();
-	    Pattern p = Pattern.compile("sourceIp=(.*), acc");//. represents single character  
-	    Matcher m = p.matcher(sRequestContext);
-	
-	    boolean b = m.find();
-	    if (b)
-	    {
-		ip = m.group(1);
-	    }
-	    LOG.info("SourceIp : " + ip);
-	    long date = System.currentTimeMillis();
-
-	    Record(r,ip,date);
+			String sRequestContext = input.get("requestContext").toString();
+			Pattern p = Pattern.compile("sourceIp=(.*), acc");//. represents single character  
+			Matcher m = p.matcher(sRequestContext);
+			
+			boolean b = m.find();
+			if (b)
+			{
+				ip = m.group(1);
+			}
+			LOG.info("SourceIp : " + ip);
+			
+			long date = System.currentTimeMillis();
+			LOG.info("date : " + date);
+			
+			Record(r,ip,date);
        }
         catch (java.io.IOException e) 
        {
